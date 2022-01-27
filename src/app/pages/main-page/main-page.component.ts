02c7268062ede6e81d13a9c9913dee5bdb5447f8
PaulRomanov <ramosha@mail.ru>
2022-01-27 13:06:00 +0300

    feat: fix main

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { AuthService } from 'src/app/services/auth.service';
+import { ModalWindowService } from 'src/app/services/modal-window.service';
 
 @Component({
   selector: 'app-main-page',
@@ -10,10 +10,14 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class MainPageComponent {
 
-  constructor(public authService: AuthService) { }
+  constructor(public modalWindowService: ModalWindowService) { }
 
   public openModal(): void {
-    this.authService.isShowModal = true;
+    this.modalWindowService.isShowModal = true;
+  }
+
+  public closeModal(): void {
+    this.modalWindowService.isShowModal = false;
   }
 
 }
